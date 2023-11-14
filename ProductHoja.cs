2afca7437398289215b316cb443a6e687103bdbf
JavierSalazar19 <javierasc2002@gmail.com>
2023-11-14 10:57:29 -0700

    Agregar archivos de proyecto.

---
+++ ProductHoja.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPatronComposite
+{
+    public class ProductoHoja : ProductoComponente
+    {
+        public string UnidadProducto { get; set; }
+        public ProductoHoja(string nombre, double precio, string unidad) : base(nombre, precio)
+        {
+            this.UnidadProducto = unidad;
+        }
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using PracticaPatronComposite;
+
+ProductoHoja producto1 = new ProductoHoja("Huevo", 3, "Pz");
+ProductoHoja producto2 = new ProductoHoja("Tocino", 50, "gr");
+ProductoHoja producto3 = new ProductoHoja("Leche", 21, "Lt");
+ProductoHoja producto4 = new ProductoHoja("Pizza Food", 80, "Pz");
+
+CarritoCompositor venta = new CarritoCompositor("Venta 1");
+venta.AgregarProducto(producto1);
+venta.AgregarProducto(producto2);
+venta.AgregarProducto(producto3);
+
+Console.WriteLine($"Total: {venta.obtenerTotal}");
+
+CarritoCompositor ventaActualizada = new CarritoCompositor("Venta 1 actualizada");
+ventaActualizada.AgregarProducto(venta);
+ventaActualizada.AgregarProducto(producto4);
+
+
+Console.WriteLine($"Se actualizo la compra \nTotal: {ventaActualizada.obtenerTotal}");
